1353975d86d584c5678566a97473538e1b9ca682
mail@keiro.fr <mail@keiro.fr@3e178732-d4e4-cf14-6a5f-ffb5eee997f9>
2010-10-19 16:08:18 +0000

    Architecture basique du projet avec configuration de Visual C++ pour HGE. Le coeur du projet se trouve dans main.cpp et permet juste d'afficher une fenêtre noire.

---
+++ gumble/gumble/main.cpp
@@ -0,0 +1,34 @@
+#include <windows.h>
+#include "hge.h"
+
+HGE *hge = 0;
+
+bool FrameFunc()
+{
+	if(hge->Input_GetKeyState(HGEK_ESCAPE))
+		return true;
+	return false;
+}
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	hge = hgeCreate(HGE_VERSION);
+	hge->System_SetState(HGE_WINDOWED, true);
+	hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
+	hge->System_SetState(HGE_TITLE, "Gumble"); 
+	hge->System_SetState(HGE_SHOWSPLASH, false);
+	hge->System_SetState(HGE_SCREENWIDTH, 800);
+	hge->System_SetState(HGE_SCREENHEIGHT, 600);
+	bool lol = true;	
+	if(hge->System_Initiate() && lol) 
+	{ 
+		hge->System_Start();
+	}
+	else
+	{
+		MessageBox(NULL, "mdr", "Error", MB_OK | MB_ICONERROR | MB_APPLMODAL);
+	}
+	hge->System_Shutdown();
+	hge->Release();
+	return 0; 
+}
