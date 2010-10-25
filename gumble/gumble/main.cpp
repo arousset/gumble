82a0aa13005b84681fdc43760b230e5536e5899d
albanrousset <albanrousset@3e178732-d4e4-cf14-6a5f-ffb5eee997f9>
2010-10-25 14:27:30 +0000

    Ajout menu (fonctionnel)

--- gumble/gumble/main.cpp
+++ gumble/gumble/main.cpp
@@ -15,67 +15,39 @@ hgeSprite* bgSprite;
 // Some resource handles
 HEFFECT				snd;
 HTEXTURE			tex;
-hgeQuad				quad;
 
-// Pointers to the HGE objects we will use
+// Pointeurs pour HGE object
 hgeGUI				*gui;
 hgeFont				*fnt;
 hgeSprite			*spr;
 
 bool FrameFunc() // Fonction appelée à chaque image
 {
-	/*if(hge->Input_GetKeyState(HGEK_ESCAPE))
-		return true;
-	return false;*/
+  int id;
+  static int lastid=0;
+  float dt=hge->Timer_GetDelta();
 
-	//debut de l'ajout
-	float dt=hge->Timer_GetDelta();
-	static float t=0.0f;
-	float tx,ty;
-	int id;
-	static int lastid=0;
+  // Si 'echap' => exit
+  if(hge->Input_GetKeyState(HGEK_ESCAPE)) { lastid=5; gui->Leave(); }
 
-	// If ESCAPE was pressed, tell the GUI to finish
-	if(hge->Input_GetKeyState(HGEK_ESCAPE)) { lastid=5; gui->Leave(); }
-	
-	// We update the GUI and take an action if
-	// one of the menu items was selected
-	id=gui->Update(dt);
-	if(id == -1)
-	{
-		switch(lastid)
-		{
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-				gui->SetFocus(1);
-				gui->Enter();
-				break;
+  id=gui->Update(dt);
+  if(id == -1)
+  {
+    switch(lastid)
+    {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+        gui->SetFocus(1);
+        gui->Enter();
+        break;
 
-			case 5: return true;
-		}
-	}
-	else if(id) { lastid=id; gui->Leave(); }
+      case 5: return true;
+    }
+  }
+  else if(id) { lastid=id; gui->Leave(); }
 
-	// Here we update our background animation
-	t+=dt;
-	tx=50*cosf(t/60);
-	ty=50*sinf(t/60);
-
-	quad.v[0].tx=tx;        quad.v[0].ty=ty;
-	quad.v[1].tx=tx+800/64; quad.v[1].ty=ty;
-	quad.v[2].tx=tx+800/64; quad.v[2].ty=ty+600/64;
-	quad.v[3].tx=tx;        quad.v[3].ty=ty+600/64;
-
-	// fin ajout
- 
- /*
- hge->Gfx_BeginScene();
- hge->Gfx_Clear(0);  //clear the screen, filling it with black
- bgSprite->RenderStretch(0, 0, 800, 600); //render the background sprite stretched
- //bgSprite->Render(0, 0);
- hge->Gfx_EndScene(); */
 
  return false;
 }
@@ -84,10 +56,8 @@ bool RenderFunc()
 {
 	// Render graphics
 	hge->Gfx_BeginScene();
-	hge->Gfx_RenderQuad(&quad);
+	bgSprite->Render(0, 0);
 	gui->Render();
-	fnt->SetColor(0xFFFFFFFF);
-	fnt->printf(5, 5, HGETEXT_LEFT, "dt:%.3f\nFPS:%d", hge->Timer_GetDelta(), hge->Timer_GetFPS());
 	hge->Gfx_EndScene();
 
 	return false;
@@ -95,34 +65,12 @@ bool RenderFunc()
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	/*hge = hgeCreate(HGE_VERSION);
-	hge->System_SetState(HGE_WINDOWED, true); // programme fenêtre ou plein écran ?
-	hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
-	hge->System_SetState(HGE_TITLE, "Gumble"); 
-	hge->System_SetState(HGE_SHOWSPLASH, false);
-	hge->System_SetState(HGE_SCREENWIDTH, 800);
-	hge->System_SetState(HGE_SCREENHEIGHT, 600);
-	hge->System_SetState(HGE_LOGFILE,"bootlog.txt");
-	
-	
-	if(hge->System_Initiate()) 
-	{ 
-		Res = new hgeResourceManager("resource.res");  
-		bgSprite = Res->GetSprite("bgSprite");
-		hge->System_Start();
-	}
-	else
-	{
-		MessageBox(NULL, hge->System_GetErrorMessage(), "Error", MB_OK | MB_ICONERROR | MB_APPLMODAL); // Erreur si le système ne s'initialise pas
-	}
-	
-	delete Res;*/
 	hge = hgeCreate(HGE_VERSION);
 
-	hge->System_SetState(HGE_LOGFILE, "hge_tut06.log");
+	hge->System_SetState(HGE_LOGFILE, "hge.log");
 	hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
 	hge->System_SetState(HGE_RENDERFUNC, RenderFunc);
-	hge->System_SetState(HGE_TITLE, "HGE Tutorial 06 - Creating menus");
+	hge->System_SetState(HGE_TITLE, "Gumble");
 	hge->System_SetState(HGE_WINDOWED, true);
 	hge->System_SetState(HGE_SCREENWIDTH, 800);
 	hge->System_SetState(HGE_SCREENHEIGHT, 600);
@@ -130,52 +78,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 	if(hge->System_Initiate())
 	{
+		// Chargement du fichier 'ressource.res'
 		Res = new hgeResourceManager("resource.res");  
+		// Chargement du background
 		bgSprite = Res->GetSprite("bgSprite");
-		//hge->System_Start();
-		// Load sound and textures
-		quad.tex=hge->Texture_Load("bg.png");
+			
+		// Chargement textures et sons
 		tex=hge->Texture_Load("cursor.png");
-		snd=hge->Effect_Load("menu.wav");
-		if(!quad.tex || !tex || !snd)
-		{
-			// If one of the data files is not found, display
-			// an error message and shutdown.
-			MessageBox(NULL, "Can't load BG.PNG, CURSOR.PNG or MENU.WAV", "Error", MB_OK | MB_ICONERROR | MB_APPLMODAL);
-			hge->System_Shutdown();
-			hge->Release();
-			return 0;
-		}
-
-		// Set up the quad we will use for background animation
-		quad.blend=BLEND_ALPHABLEND | BLEND_COLORMUL | BLEND_NOZWRITE;
-
-		for(int i=0;i<4;i++)
-		{
-			// Set up z-coordinate of vertices
-			quad.v[i].z=0.5f;
-			// Set up color. The format of DWORD col is 0xAARRGGBB
-			quad.v[i].col=0xFFFFFFFF;
-		}
-
-		quad.v[0].x=0; quad.v[0].y=0; 
-		quad.v[1].x=800; quad.v[1].y=0; 
-		quad.v[2].x=800; quad.v[2].y=600; 
-		quad.v[3].x=0; quad.v[3].y=600; 
-
-
-		// Load the font, create the cursor sprite
-		fnt=new hgeFont("font2.fnt");
+		snd=hge->Effect_Load("boing_2.wav");
+		
+		// Chargement de la police et du sprite du cursor
+		fnt=new hgeFont("font1.fnt");
 		spr=new hgeSprite(tex,0,0,32,32);
 
-		// Create and initialize the GUI
+		// Initialise le GUI
 		gui=new hgeGUI();
 
-		gui->AddCtrl(new hgeGUIMenuItem(1,fnt,snd,400,200,0.0f,"Play"));
-		gui->AddCtrl(new hgeGUIMenuItem(2,fnt,snd,400,240,0.1f,"Options"));
-		gui->AddCtrl(new hgeGUIMenuItem(3,fnt,snd,400,280,0.2f,"Instructions"));
-		gui->AddCtrl(new hgeGUIMenuItem(4,fnt,snd,400,320,0.3f,"Credits"));
-		gui->AddCtrl(new hgeGUIMenuItem(5,fnt,snd,400,360,0.4f,"Exit"));
+		gui->AddCtrl(new hgeGUIMenuItem(1,fnt,snd,385,360,0.0f,"Play"));
+		gui->AddCtrl(new hgeGUIMenuItem(2,fnt,snd,385,400,0.1f,"Options"));
+		gui->AddCtrl(new hgeGUIMenuItem(3,fnt,snd,385,440,0.2f,"Instructions"));
+		gui->AddCtrl(new hgeGUIMenuItem(4,fnt,snd,385,480,0.3f,"Credits"));
+		gui->AddCtrl(new hgeGUIMenuItem(5,fnt,snd,385,520,0.4f,"Exit"));
 
 		gui->SetNavMode(HGEGUI_UPDOWN | HGEGUI_CYCLED);
 		gui->SetCursor(spr);
@@ -185,14 +108,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		// Let's rock now!
 		hge->System_Start();
 
-		// Delete created objects and free loaded resources
+		// Libération des ressoureces
 		delete gui;
-		delete fnt;
-		delete spr;
-		hge->Effect_Free(snd);
-		hge->Texture_Free(tex);
-		hge->Texture_Free(quad.tex);
+	    delete fnt;
+	    delete spr;
+	    hge->Texture_Free(tex);
+	    hge->Effect_Free(snd);
 	}
+	
 	hge->System_Shutdown();
 	hge->Release();
 	return 0; 
