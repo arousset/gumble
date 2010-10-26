5dc066462911598efea2d2231577f1b00830b90d
mail@keiro.fr <mail@keiro.fr@3e178732-d4e4-cf14-6a5f-ffb5eee997f9>
2010-10-26 01:09:00 +0000

    Ajouts :    - lecture du fichier map (map.txt)             - Affichage des boules selon leur couleurs et leur position dans le fichier map             - Mise en place du système pour décaler une ligne de boules sur deux (adaptation pour la descente)             - Mise en place du système de temps pour la descente des boules, ainsi que de leur descente progressive             - Remise en place de la touche échap pour quitter le programme (pour faciliter le temps de tests)             - Désactivation des animations sur les boules (elles ne tournent plus) car ceci n'était pas très joli car bien calibré.

--- gumble/gumble/main.cpp
+++ gumble/gumble/main.cpp
@@ -7,10 +7,23 @@
 #include <math.h>
 #include "hgefont.h"
 #include "hgegui.h"
+#include <stdio.h>
 
 HGE *hge = 0;
 hgeResourceManager* Res;
 hgeSprite* bgSprite;
+char* pMap = NULL; // pointeur sur la desctiption de la map
+int sizeX, sizeY;
+float fScale =27;
+const int xMap = 290; // abscisse de la plus basse ligne pour afficher les boules
+const int yMap = 400; // ordonnée de la plus basse ligne pour afficher les boules
+const float bouleSize = 40; // taille de la boule
+float timeDown = 3; // temps avant de faire tomber les boules(secondes)
+float timeCpt = 0; // compteur de temps
+int swapPair = 0; // variables utile pour déterminée si la ligne a besoin d'etre décalée
+bool isDowning = false; // les boules sont en train de descendre ?
+float animDowning = 1; // utile pour faire une sorte d'animation lors de la descente des boules
+
 
 // Sprite pour les boules
 hgeAnimation* b_rouge; //rouge
@@ -34,7 +47,8 @@ hgeSprite			*spr;
 bool FrameFunc() // Fonction appelée à chaque image
 {
 	
-
+	if(hge->Input_GetKeyState(HGEK_ESCAPE))
+		return true;
  /* int id;
   static int lastid=0;
   float dt=hge->Timer_GetDelta();
@@ -67,18 +81,133 @@ bool FrameFunc() // Fonction appelée à chaque image
 bool RenderFunc()
 {
 	float dt=hge->Timer_GetDelta();  //get the time since the last call to FrameFunc
-	b_rouge->Update(dt);  //update the animation
+	timeCpt += dt; // on additionne les temps entre les images
+	if(timeCpt > timeDown) // si ce compteur est supérieur aux temps définis pour faire tomber les boules :
+	{													// on actualise pMap pour lui enlever une ligne en bas (la ligne la plus basse disparait pour faire descendre les autres)
+		if(!isDowning) // les boules ne sont pas en train de descendre
+		{
+			sizeY -= 1; // nouveau nombre de ligne (on supprime celle en bas)
+			char* pMapTmp = new char[(sizeX*sizeY)];		// pour cela, on créé un nouveau tableau		
+			for(int cpt = 0; cpt < (sizeX*sizeY);cpt++)   // puis on recopie les valeurs de pMap en enlevant la premiere ligne
+			{
+				pMapTmp[cpt] = pMap[cpt+7];
+			}
+			char* toFree = pMap;
+			pMap = pMapTmp;
+			free(toFree); // on free l'ancien tableau
+			timeCpt = 0; // on remet le compteur de temps à 0;
+			swapPair = (swapPair+1)%2; // on modifie la variable a chaque descente de boules (elle prend soit 1 soit 0)
+			isDowning = true;
+		}
+	}
+
+	/*b_rouge->Update(dt);  //update the animation
+	b_vert->Update(dt);
+	b_bleu->Update(dt);
+	b_orange->Update(dt);*/
 
 	// Render graphics
 	hge->Gfx_BeginScene();
 	bgSprite->Render(0, 0);
 	//b_rouge->Render(250, 70);
-	b_rouge->RenderStretch(250,70,290,110);
+	//b_rouge->RenderStretch(250,70,290,110);
 	//gui->Render();
-	
-	hge->Gfx_EndScene();
+	for(int y=0; y<sizeY; ++y)
+	{
+		float posY =y*fScale;
+		for(int x=0; x<sizeX; ++x)
+		{
+			if(y < 9)
+			{
+				if(y == 0 && x == 0 && isDowning) // si les boules sont en train de descendre, on leur attribut pour chaque image une position décalée de 0.05 px (descente petit à petit)
+					animDowning += 0.05;
 
+
+				float decalage = 0.0;       // si la ligne est paire, on la décale
+				if((y+swapPair)%2 == 0)
+					decalage = bouleSize/2;
+				/*
+				On utilise swapPair pour regarder si on doit décaler ou non la ligne
+				Exemple : premieres secondes du jeu, la ligne 5 est impaire et la ligne 6 est paire
+				On affiche donc la ligne 26 décalée à droite par rapport à la ligne 5 (selon les modalités du jeu)
+
+				Ensuite, durant la deuxième phase (les boules descendent d'un cran), la ligne 5 devient la ligne 4 (donc paire) et la ligne 6 devient la ligne 5 (donc impaire)
+				on doit donc dire à l'affichage de ne plus décaler les lignes paires, mais impaires pour que les lignes restent toujours au même endroit en ordonnées.
+				La variable swapPair sert alterner le modulo 2 du numéro de ligne pour que celui ci reste contant selon la ligne.
+				*/
+
+				// Nouvelles positions pour l'affichages des boules
+				float x1 = xMap+((x-1)*bouleSize+decalage);
+				float y1 = yMap-((y-1)*bouleSize);
+				float x2 = xMap+bouleSize+((x-1)*bouleSize+decalage);
+				float y2 = yMap-bouleSize-((y-1)*bouleSize);
+				if(isDowning)
+				{
+					if(animDowning < bouleSize)
+					{
+						// Modification des coordonnées des boules si elles sont en train de descendre (descente progressive sur les y)
+						y1 -= (bouleSize-animDowning);
+						y2 -= (bouleSize-animDowning);
+					}
+					else
+					{
+						animDowning = 1;
+						isDowning = false;
+						timeCpt = 0;
+					}
+				}
+
+				float posX =x*fScale;
+
+				int index =y*sizeX+x;
+
+				switch (pMap[index])
+				{
+					 case 'r': {
+						  b_rouge->RenderStretch(x1, y1, x2, y2);break;
+					}
+					case 'v': {
+						  b_vert->RenderStretch(x1, y1, x2, y2);break;
+					}
+					case 'b': {
+						  b_bleu->RenderStretch(x1, y1, x2, y2);break;
+					}
+					case 'o': {
+						  b_orange->RenderStretch(x1, y1, x2, y2);break;
+					}
+			   }
+			}
+		}
+	}	
+	hge->Gfx_EndScene();
 	return false;
+}
+
+void LoadMap()
+{
+	FILE* f = NULL;
+	f = fopen("../Debug/map.txt", "rb"); // ouverture du fichier map
+	if(f == NULL)
+		return;
+
+	char line[512];
+	fgets(line, sizeof(line),f);
+	sizeX  =atoi(line); // nombre de colones
+	fgets(line, sizeof(line),f);
+	sizeY = atoi(line); // nombres de lignes
+
+	//!\\ La première ligne de la map est la ligne se tranvant le plus en bas sur le jeu. La dernière ligne de la map représente donc la ligne de boules le plus en haut possible
+
+	pMap = new char[sizeX*sizeY]; // la pointeur sur la map réserve la taille de la map en mémoire
+
+	for(int iline=0; iline<sizeY; ++iline) // copies des informations du fichier map
+	{
+		fgets(line, sizeof(line),f);
+		memcpy(&pMap[iline*sizeX], line, sizeX);
+	}
+
+	fclose(f);
+	
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -89,6 +218,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
 	hge->System_SetState(HGE_RENDERFUNC, RenderFunc);
 	hge->System_SetState(HGE_TITLE, "Gumble");
+	hge->System_SetState(HGE_SHOWSPLASH, false);
 	hge->System_SetState(HGE_WINDOWED, true);
 	hge->System_SetState(HGE_SCREENWIDTH, 800);
 	hge->System_SetState(HGE_SCREENHEIGHT, 600);
@@ -100,8 +230,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		Res = new hgeResourceManager("resource.res");  
 		// Chargement du background
 		bgSprite = Res->GetSprite("bgSprite");
-		b_rouge = Res->GetAnimation("b");
+		b_rouge = Res->GetAnimation("br");
+		b_vert = Res->GetAnimation("bv");
+		b_bleu = Res->GetAnimation("bb");
+		b_orange = Res->GetAnimation("bo");
+
 		b_rouge->Play();  //start playback of animation
+		b_vert->Play();
+		b_bleu->Play();
+		b_orange->Play();
+		LoadMap(); // charge la map
 
 		// Chargement textures et sons
 		//tex=hge->Texture_Load("cursor.png");
@@ -116,8 +254,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		/*gui=new hgeGUI();
 
 		gui->AddCtrl(new hgeGUIMenuItem(1,fnt,snd,385,360,0.0f,"Play"));
-		gui->AddCtrl(new hgeGUIMenuItem(2,fnt,snd,385,400,0.1f,"Options"));
-		gui->AddCtrl(new hgeGUIMenuItem(3,fnt,snd,385,440,0.2f,"Instructions"));
+		gui->AddCtrl(new hgeGUIMenuItem(2,fnt,snd,385,bouleSize0,0.1f,"Options"));
+		gui->AddCtrl(new hgeGUIMenuItem(3,fnt,snd,385,4bouleSize,0.2f,"Instructions"));
 		gui->AddCtrl(new hgeGUIMenuItem(4,fnt,snd,385,480,0.3f,"Credits"));
 		gui->AddCtrl(new hgeGUIMenuItem(5,fnt,snd,385,520,0.4f,"Exit"));
 
@@ -136,7 +274,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	    hge->Texture_Free(tex);
 	    hge->Effect_Free(snd);*/
 	}
-	
+	free(pMap);
 	hge->System_Shutdown();
 	hge->Release();
 	return 0; 
